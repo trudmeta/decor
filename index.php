40937615bfffb56b919d379fd84ea0f0dbc9f289
eugenik <trudmeta@gmail.com>
2021-04-05 17:45:37 +0300

    init

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Сияние</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    <link rel="icon" href="favicon.ico" type="image/x-icon">-->
+    <link rel="stylesheet" href="/css/fullpage.min.css">
+    <link rel="stylesheet" href="/css/mmenu.css" media="screen and (max-width: 1280px)">
+    <link rel="stylesheet" href="/css/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="/css/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+
+
+<div class="main main-page">
+
+    <header class="header-mob">
+        <div class="header-left">
+            <div class="hamburger">
+                <div class="hamburger-box">
+                    <div class="hamburger-inner"></div>
+                </div>
+            </div>
+            <div class="logo">
+                <h1><a href="/">Сияние</a></h1>
+            </div>
+        </div>
+
+        <div class="phone header-phone-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 15 15">
+                <image data-name="Слой 1" width="15" height="15" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAA7klEQVQokZXSvyvGURTH8RdJfpRSBgYTk39A2SwGm5HJP2AwGFhMJoq/gOI/YJKS2J5J35WJRdmkLESn7lO3+3zVfT7D/X7u+Z53955z7kDTNCo1jg4mcIG9oVoSq1hIfhczgz0p/2ut+LNZC09hvYh1auGxnggPtfBL1Ji+XV32U/MbhpN/x30/8DWmkz+MpRbex0rynzgOU8459j/4zWIH8SCy/Qa+c3gpzTHG8YXF1OFzLGfgEa7yk26LhNAzRjGSxU6wU16zBEOTmf/ANk7LpGjYWQscek31zreB3ZO3MIs5POERd7jpNqZV+ANGkSaYzOFJ9QAAAABJRU5ErkJggg=="/>
+            </svg>
+        </div>
+    </header>
+
+    <nav id="mmenu">
+        <header class="header-desktop">
+            <div class="logo">
+                <h1><a href="/">Сияние</a></h1>
+                <span>декор центр</span>
+            </div>
+
+            <div class="header-text">Интернет-магазин отделочных материалов и дизайна.</div>
+        </header>
+
+        <ul>
+            <li>
+                <a href="#">Каталог товаров</a>
+            </li>
+            <li>
+                <a href="/news/">Новости и акции</a>
+                <ul>
+                    <li><a href="/news/history/">History</a></li>
+                    <li><a href="/news/team/">The team</a></li>
+                    <li><a href="/news/address/">Our address</a></li>
+                </ul>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="23" viewBox="0 0 34 23">
+                    <defs>
+                        <style>
+                            .cls-1 {
+                                fill: #b36060;
+                                fill-rule: evenodd;
+                            }
+                        </style>
+                    </defs>
+                    <path data-name="Прямоугольник 9" class="cls-1 flag" d="M276,414h34v23H276l10-12Z" transform="translate(-276 -414)"/>
+                    <text x="60%" y="55%" dominant-baseline="middle" text-anchor="middle" style="fill: #fff;">1</text>
+                </svg>
+            </li>
+            <li>
+                <a href="#">Наши услуги</a>
+            </li>
+            <li>
+                <a href="#">Портфолио</a>
+            </li>
+            <li>
+                <a href="#">Блог</a>
+            </li>
+            <li>
+                <a href="#">Контакты</a>
+            </li>
+
+            <li class="bottom bottom-first"><a href="#">О компании</a></li>
+            <li class="bottom"><a href="#">Отзывы о нас</a></li>
+            <li class="bottom"><a href="#">Доставка и оплата</a></li>
+            <li class="bottom"><a href="#">Дизайнеры</a></li>
+        </ul>
+
+        <div class="block-phone">
+            <ul class="block-phone-list">
+                <li class="bottom bottom-first"><a href="#">О компании</a></li>
+                <li class="bottom"><a href="#">Отзывы о нас</a></li>
+                <li class="bottom"><a href="#">Доставка и оплата</a></li>
+                <li class="bottom"><a href="#">Дизайнеры</a></li>
+            </ul>
+            <p><a href="tel:+74822648080">+7 (4822) 648-080</a></p>
+            <button class="block-phone-btn">Заказать звонок</button>
+        </div>
+    </nav>
+
+    <div id="fullpage">
+        <!--slider-->
+        <div class="section">
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <div class="slide-bg" style="background-image: url(/images/slider/slide1.jpg)"></div>
+                    <div class="slide-content">
+                        <div class="slide-content-text">
+                            <p><strong>Декоративные покрытия</strong></p>
+                            <p>от лучших производителей</p>
+                        </div>
+                        <a href="#" class="slide-btn">
+                            <span>Подробнее</span>
+                            <i class="arrow"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="slide-bg" style="background-image: url(/images/slider/slide2.jpg)"></div>
+                    <div class="slide-content">
+                        <div class="slide-content-text">
+                            <p><strong>Напольные покрытия</strong></p>
+                        </div>
+                        <a href="#" class="slide-btn">
+                            <span>Подробнее</span>
+                            <i class="arrow"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--content-->
+        <div class="section">
+            <div class="intro">
+                <h2>Scrolling inside sections</h2>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                        <div class="col"><div class="content"></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="modal modal-feedback">
+    <div class="popup-overflow"></div>
+
+    <div class="popup popup-feedback">
+        <div class="popup-wrapper">
+            <div class="popup-img">
+                <img src="/images/img1.jpg" alt="">
+            </div>
+            <div class="popup-content">
+                <span class="popup-close"></span>
+
+                <h3>Обратная связь</h3>
+
+                <form action="" class="feedback-form">
+                    <div class="form-group">
+                        <label for="feedback_email">
+                            E-mail <i>*</i>
+                        </label>
+                        <input id="feedback_email" type="email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="feedback_phone">
+                            Телефон <i>*</i>
+                        </label>
+                        <input id="feedback_phone" type="text" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="feedback_message">
+                            Сообщение
+                        </label>
+                        <textarea name="feedback_message" id="feedback_message"></textarea>
+                    </div>
+                    <div class="form-group form-group-submit">
+                        <button type="submit">Отправить</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal modal-feedback-success">
+    <div class="popup-overflow"></div>
+
+    <div class="popup popup-success">
+        <div class="popup-wrapper">
+            <div class="popup-img">
+                <img src="/images/img2.jpg" alt="">
+            </div>
+            <div class="popup-content">
+                <span class="popup-close"></span>
+
+                <h3>Ваше сообщение отправлено!</h3>
+
+                <p>В ближайшее время с вами свяжется наш менеджер</p>
+
+                <a href="/"> На главную</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script src="/js/jquery-3.6.0.min.js"></script>
+<script src="/js/scrolloverflow.min.js"></script>
+<script src="/js/fullpage.min.js"></script>
+<script src="/js/mmenu.polyfills.js"></script>
+<script src="/js/mmenu.js"></script>
+<script src="/js/owl.carousel.min.js"></script>
+<script src="/js/sliiide.min.js"></script>
+<script src="/js/script.js"></script>
+
+
+</body>
+</html>
